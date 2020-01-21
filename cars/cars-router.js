77dcaac605e503aafb904d7fe3a98e328bf95508
John Afolabi <johnafolabi.da@gmail.com>
2020-01-21 18:08:15 +0100

    Add middlewares

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const {
   get,
-  getCarbyId,
   addCar,
   updateCar,
   removeCar
 } = require("./cars-model");
+const { validateCarId, validatePostData } = require("../middlewares/index");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -20,19 +20,11 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
-  getCarbyId(req.params.id)
-    .then(car => {
-      res.status(200).json(car);
-    })
-    .catch(error => {
-      res.status(500).json({
-        errorMessage: `Could not retrieve the car with ID:${id} at this moment`
-      });
-    });
+router.get("/:id", validateCarId, (req, res) => {
+  res.status(200).json(req.car);
 });
 
-router.post("/", (req, res) => {
+router.post("/", validatePostData, (req, res) => {
   addCar(req.body)
     .then(caradded => {
       res.status(201).json(caradded);
@@ -44,7 +36,7 @@ router.post("/", (req, res) => {
     });
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", validateCarId, validatePostData, (req, res) => {
   updateCar(req.params.id, req.body)
     .then(car => {
       res.status(201).json(car);
@@ -56,7 +48,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", validateCarId, (req, res) => {
   removeCar(req.params.id)
     .then(numDel => {
       res.status(200).json({ message: `${numDel} car has been deleted` });
@@ -67,6 +59,5 @@ router.delete("/:id", (req, res) => {
         .json({ errorMessage: `Unable to delete car at this moment` });
     });
 });
-
 
 module.exports = router;
